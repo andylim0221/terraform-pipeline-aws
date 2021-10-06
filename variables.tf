7b0f61f2c4177f0cca92766e1e72bc9f7f0fdced
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-10-06 14:09:01 +0800

    Updated HCL.

--- variables.tf
+++ variables.tf
@@ -8,3 +8,25 @@ variable "api_name" {
   #    error_message = "Please provide a valid value for variable AMI."
   #  }
 }
+
+variable "waf_name" {
+  type        = string
+  description = "The name of the Lambda function."
+  default     = "StandardACL_Regional"
+  #  sensitive   = true
+  #  validation {
+  #    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+  #    error_message = "Please provide a valid value for variable AMI."
+  #  }
+}
+
+variable "lambda_name" {
+  type        = string
+  description = "The name of the Lambda function."
+  default     = "tf_lambda"
+  #  sensitive   = true
+  #  validation {
+  #    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+  #    error_message = "Please provide a valid value for variable AMI."
+  #  }
+}
