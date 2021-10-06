7bdf0e8ee9e40e107a47093f2cf641fa119dbf4b
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-10-06 14:11:55 +0800

    Updated HCL.

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.function.lambda_invoke_arn
+  uri                     = aws_lambda_function.function.invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "response_200" {
