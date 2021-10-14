675383a635832a9299d7a9cf0a34cb04a28ceb3b
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-10-14 11:46:05 +0800

    Updated Lambda TF.

--- modules/security_demo/lambda.tf
+++ modules/security_demo/lambda.tf
@@ -19,11 +19,11 @@ EOF
 
 resource "aws_lambda_function" "function" {
   architectures    = ["arm64"]
-  filename         = "./resources/lambda_function.zip"
+  filename         = "./modules/security_demo/resources/lambda_function.zip"
   function_name    = var.lambda_name
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = filebase64sha256("./resources/lambda_function.zip")
+  source_code_hash = filebase64sha256("./modules/security_demo/resources/lambda_function.zip")
   runtime          = "python3.9"
   environment {
     variables = {
