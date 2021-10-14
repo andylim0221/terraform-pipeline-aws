a536c8b2766112fe1a3e2b85494d7c2212331a55
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-10-14 11:52:34 +0800

    Destroying everything for re-genesis.

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # Create modules for your projects and call them here.
 
 ## Security demonstrator API.
-module "security_demo" {
-  source = "./modules/security_demo"
-}
-output "api_endpoint" {
-  value     = module.security_demo.api_endpoint
-  sensitive = false
-}
+# module "security_demo" {
+#   source = "./modules/security_demo"
+# }
+# output "api_endpoint" {
+#   value     = module.security_demo.api_endpoint
+#   sensitive = false
+# }
