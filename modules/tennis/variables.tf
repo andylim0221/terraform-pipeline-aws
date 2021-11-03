628e7977565d1f4017eb2fb5483039b31bb988d0
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-11-03 21:22:58 +0800

    Added intrinsic variables to tennis module.

---
+++ modules/tennis/variables.tf
@@ -0,0 +1,7 @@
+# Declare intrinsic variables.
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+}
