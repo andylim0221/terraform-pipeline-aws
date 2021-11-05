0f06f9803ed92f69880929db9a3b27674e2e303a
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-11-06 01:51:55 +0800

    Updated backend config.

--- aws.tf
+++ aws.tf
@@ -9,8 +9,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # version = "~> 3.0"
-      version = ">= 3.64.1"
+      version = ">= 3.64.1" # version = "~> 3.0"
     }
   }
   required_version = ">= 1.0.10"
